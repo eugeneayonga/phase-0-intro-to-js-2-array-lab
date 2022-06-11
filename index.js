f7e1979d68ed7f446cb4213055ef3e8a9f00dbf8
Eugene <eugeneayonga@gmail.com>
2022-06-11 18:34:09 +0300

    commit tree dd531d6dcb13d1ffc1d2b3e7b5bd6015dbed9537

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const catA = [...cats, "Broom"];
+    return catA;
+}
+
+function prependCat(name) {
+    const catsB = ["Arnold", ...cats];
+    return catsB;
+}
+
+function removeLastCat(name) {
+    const catsC = cats.slice(0, cats.length -1);
+    return catsC;
+}
+
+function removeFirstCat() {
+    const catsD = cats.slice(1);
+    return catsD;
+}
